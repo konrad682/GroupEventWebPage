5e533c7992e21def75d96c2799516d18edde7be6
Konrad Wędzicha <konrad.wedzicha@relativity.com>
2020-10-19 15:11:45 +0200

    Refactor tsconfig

--- ClientApp/webpack.config.js
+++ ClientApp/webpack.config.js
@@ -50,11 +50,5 @@ module.exports = {
     ],
     devServer: {
         historyApiFallback: true
-    },
-    output: {
-        path: helpers.root('dist'),
-        publicPath: '/',
-        filename: '[name].bundle.js',
-        chunkFilename: '[id].chunk.js'
     }
 }
